8d8282c49c2bb2ac16827be1d7b6f71ebf7634fc
Joseph Spagnuolo <josephspagnuolo1@gmail.com>
2024-08-01 02:45:53 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -24,7 +24,7 @@ def doDB():
     result = []
     try:
         cursor = connection.cursor()
-        cursor.execute("CREATE TABLE mytest (id INTEGER PRIMARY KEY)")
+        cursor.execute("CREATE TABLE IF NOT EXISTS mytest (id INTEGER PRIMARY KEY)")
         cursor.execute("INSERT INTO mytest (id) VALUES (1), (2)")
         cursor.execute("SELECT * FROM mytest")
         result = cursor.fetchall()
