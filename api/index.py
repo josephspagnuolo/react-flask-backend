22aca8a45f33e54d7337887036ef36cb75c14879
Joseph Spagnuolo <josephspagnuolo1@gmail.com>
2024-08-01 02:22:51 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,23 +1,48 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 import os
+import pymysql
 
 app = Flask(__name__)
 cors = CORS(app, origins='*')
 
-def math(number1, number2):
-    return number1 + number2
+timeout = 10
+connection = pymysql.connect(
+    charset="utf8mb4",
+    connect_timeout=timeout,
+    cursorclass=pymysql.cursors.DictCursor,
+    db="defaultdb",
+    host=os.getenv('HOST'),
+    password=os.getenv('PASSWORD'),
+    read_timeout=timeout,
+    port=16562,
+    user="avnadmin",
+    write_timeout=timeout,
+)
+
+def doDB():
+    result = []
+    try:
+        cursor = connection.cursor()
+        cursor.execute("CREATE TABLE mytest (id INTEGER PRIMARY KEY)")
+        cursor.execute("INSERT INTO mytest (id) VALUES (1), (2)")
+        cursor.execute("SELECT * FROM mytest")
+        result = cursor.fetchall()
+    finally:
+        cursor.close()
+        connection.close()
+    return result
 
 @app.route('/')
 def home():
     if os.getenv('TESTING'):
-        result = math(3, 4)
+        result = doDB()
         return jsonify(
             {
-                "testing": [
-                    os.getenv('TESTING'),
-                    result
-                ]
+                "testing": {
+                    "env": testing_env,
+                    "db_result": result
+                }
             }
         )
     else:
