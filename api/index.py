3dcb40fe0db05c91eb044b41529a9a77ace84145
Joseph Spagnuolo <josephspagnuolo1@gmail.com>
2024-08-01 01:59:32 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,11 +5,20 @@ import os
 app = Flask(__name__)
 cors = CORS(app, origins='*')
 
+def math(number1, number2):
+    return number1 + number2
+
 @app.route('/')
 def home():
     if os.getenv('TESTING'):
+        result = math(3, 4)
         return jsonify(
-            { "testing":  os.getenv('TESTING') }
+            {
+                "testing": {
+                    os.getenv('TESTING'),
+                    result
+                }
+            }
         )
     else:
         return 'Hello, World!'
